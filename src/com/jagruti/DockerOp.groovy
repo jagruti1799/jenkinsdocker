84e3e9c53ec9229f88aba159689089ba8799eb15
jagruti1799 <135jagrutigaur99@gmail.com>
2022-09-12 17:00:42 +0530

    first commit

---
+++ src/com/jagruti/DockerOp.groovy
@@ -0,0 +1,19 @@
+package com.jagruti
+
+class DockerOp
+{
+    def dockerBuild()
+    {
+        print("hello Build")
+    }
+
+    def dockerPush()
+    {
+        print("hello push")
+    }
+
+    def dockerDeploy()
+    {
+        print("hello Deploy")
+    }
+}
